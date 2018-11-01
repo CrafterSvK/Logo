29e5a66397f343348ada25c009a3b213da89308f
Jakub Janek <shaman.janek@gmail.com>
2018-11-02 00:08:36 +0100

    Support for nested repeats

--- sketch.js
+++ sketch.js
@@ -24,14 +24,19 @@ function goTurtle() {
 }
 
 function interpret(command) {
-	/* Cannot nest repeat in repeat. Needs better regex. */
 	if (command.includes('repeat')) {
 		let repeats = command.match(/[0-9]+/)[0];
 
 		for (let i = 1; i <= repeats; i++) {
-			let values = command.match(/(pu)|(pd)|(([^repeat][a-z])+\ [0-9]+)/gi);
+			let com = command.match(/\[.*\]/)[0];
+
+			if (com.includes('repeat')) {
+				interpret(com.match(/[^[].*(?=])/)[0]);
+			} else {
+				let values = command.match(/(pu)|(pd)|(([^repeat][a-z])+\ [0-9]+)/gi);
 			
-			for (let value of values) interpret(value);
+				for (let value of values) interpret(value);
+			}
 		}
 	} else {
 		let values = command.split(" ");
